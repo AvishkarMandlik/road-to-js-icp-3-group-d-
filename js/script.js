75c5a10d343d7fa402acf8a723029c9f4f5825b1
Satyam Pund <pundsatyam@gmail.com>
2022-11-14 04:05:46 +0530

    add login page

--- js/script.js
+++ js/script.js
@@ -27,3 +27,33 @@ var swiper = new Swiper(".slide-content", {
     },
   },
 });
+
+/* Login page JS */
+const modal = document.querySelector(".modal-login");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal-button");
+const btnShowModal = document.querySelector("#login-btn");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+btnShowModal.addEventListener("click", openModal)
+
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    if (!modal.classList.contains("hidden")) {
+      closeModal();
+    }
+  }
+})
