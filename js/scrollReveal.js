f00b57646ac7d60b96b3615acf7e26a3c01e2b49
Satyam Pund <pundsatyam@gmail.com>
2022-12-23 11:54:23 +0530

    add animations

---
+++ js/scrollReveal.js
@@ -0,0 +1,14 @@
+ScrollReveal().reveal('.sub-heading, .btn-warning,.btn-outline-warning, .project-heading ', {
+  delay: 500,
+  origin: 'left',
+});
+
+ScrollReveal().reveal('.js-tut-img', {
+  delay: 500,
+  origin: 'bottom',
+});
+
+ScrollReveal().reveal('.reveal-card', {
+  delay: 600,
+  origin: 'bottom',
+});
